47e78b4e57053c7a3cb0186224680fbf805aecad
secretjc <jiang486@purdue.edu>
2021-07-08 11:07:42 -0400

    Update copy_ssh.sh

--- other_files/copy_ssh.sh
+++ other_files/copy_ssh.sh
@@ -1,8 +1,14 @@
 cd ~/
-ssh ms0714.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
-ssh ms0721.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
-ssh ms0712.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0344.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0329.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0313.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0338.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0310.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
+ssh ms0305.utah.cloudlab.us cat .ssh/id_rsa.pub >> tmp.tmp
 
-cat tmp.tmp | ssh ms0714.utah.cloudlab.us "cat >> .ssh/authorized_keys"
-cat tmp.tmp | ssh ms0721.utah.cloudlab.us "cat >> .ssh/authorized_keys"
-cat tmp.tmp | ssh ms0712.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0344.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0329.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0313.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0338.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0310.utah.cloudlab.us "cat >> .ssh/authorized_keys"
+cat tmp.tmp | ssh ms0305.utah.cloudlab.us "cat >> .ssh/authorized_keys"
