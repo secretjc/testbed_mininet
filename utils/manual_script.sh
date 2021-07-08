3391ae1b63d4085f7144a391efe25ecdd6ec2dfa
secretjc <jiang486@purdue.edu>
2021-07-08 18:55:39 -0400

    Update manual_script.sh

--- utils/manual_script.sh
+++ utils/manual_script.sh
@@ -1,4 +1,4 @@
-echo -e "000000\n000000\n" | sudo passwd secretjc
+echo -e "111111\n111111\n" | sudo passwd secretjc
 sudo scp testbed_mininet/other_files/sshd_config /etc/ssh/sshd_config
 sudo service ssh restart
 sudo scp testbed_mininet/other_files/sudoers /etc/sudoers
@@ -7,6 +7,9 @@ ssh-keygen -t rsa -b 4096 -C "chuanjiang93@gmail.com"
 ssh-copy-id secretjc@ms0333.utah.cloudlab.us
 ssh-copy-id secretjc@ms0301.utah.cloudlab.us
 ssh-copy-id secretjc@ms0325.utah.cloudlab.us
+ssh-copy-id secretjc@ms0338.utah.cloudlab.us
+ssh-copy-id secretjc@ms0310.utah.cloudlab.us
+ssh-copy-id secretjc@ms0305.utah.cloudlab.us
 ssh-copy-id secretjc@ms0338.utah.cloudlab.us
 ssh-copy-id secretjc@ms0310.utah.cloudlab.us
 ssh-copy-id secretjc@ms0305.utah.cloudlab.us
