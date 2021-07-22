834cbcea0994e574a9a626229156ef32a1e5aa4d
secretjc <jiang486@purdue.edu>
2021-07-22 18:05:13 -0400

    Update manual_script.sh

--- utils/manual_script.sh
+++ utils/manual_script.sh
@@ -4,9 +4,9 @@ sudo service ssh restart
 sudo scp testbed_mininet/other_files/sudoers /etc/sudoers
 
 ssh-keygen -t rsa -b 4096 -C "chuanjiang93@gmail.com"
-ssh-copy-id secretjc@ms0721.utah.cloudlab.us
-ssh-copy-id secretjc@ms0744.utah.cloudlab.us
-ssh-copy-id secretjc@ms0735.utah.cloudlab.us
-ssh-copy-id secretjc@ms0724.utah.cloudlab.us
-ssh-copy-id secretjc@ms0702.utah.cloudlab.us
-ssh-copy-id secretjc@ms0712.utah.cloudlab.us
+ssh-copy-id secretjc@ms0234.utah.cloudlab.us
+ssh-copy-id secretjc@ms0216.utah.cloudlab.us
+ssh-copy-id secretjc@ms0226.utah.cloudlab.us
+ssh-copy-id secretjc@ms0235.utah.cloudlab.us
+ssh-copy-id secretjc@ms0202.utah.cloudlab.us
+ssh-copy-id secretjc@ms0242.utah.cloudlab.us
