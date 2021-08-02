086533a1116215f0ba900e21607bf522bcfefdac
zixuanli3566 <li3566@purdue.edu>
2021-08-02 17:17:13 -0400

    auto run script

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,48 @@
 #!/usr/bin/env bash
+server0=ms0626.utah.cloudlab.us
+username=li3566
 
-scenario_num=($(python ./utils/read_scenario.py | tr -d '[],'))
+serverfile=./utils/server.txt
+
+
+
+declare -a serverList
+serverList=(`cat "$serverfile"`)
+
+
+scenario_num=($(python ./utils/read_scenario.py --scenario_file ./_data/scenario_ibm.tab| tr -d '[],'))
 echo $scenario_num
+
+ssh ${username}@${server0} "
+cd ~/testbed_mininet
+mkdir iperf_results
+"
+
+
 for i in "${scenario_num[@]}"
 do
-   python main.py --main_config ./_config/main.yaml --topo_config ./_config/scenario_${i}.yaml
+    ssh -t ${username}@${server0} "
+    cd ~/testbed_mininet
+    mkdir iperf_results/ibm_scenario_${i}
+    sudo python main.py --main_config ./_config/main.yaml --topo_config ./_config/scenario_${i}.yaml
+    "
+    wait
+    ssh ${username}@${server0} "
+    cd ~/testbed_mininet
+    mv *.txt iperf_results/ibm_scenario_${i}
+    "
+    for ((x=1; x<${#serverList[@]}; x++));
+    do
+        echo "${serverList[x]}"
+        scp ${username}@${serverList[x]}:~/*.txt ${username}@${server0}:~/testbed_mininet/iperf_results/ibm_scenario_${i}
+        ssh ${username}@${serverList[x]} "
+        rm ~/*.txt"
+    done
+    scp -r ${username}@${server0}:~/testbed_mininet/iperf_results/ibm_scenario_${i} . &
+    for server in ${serverList[*]}
+    do
+        echo "$server"
+        ssh ${username}@${server} "
+        sudo mn -c"
+    done
 done
