8d88e43fda0eb9c07d302de0f669b8652b4e8aea
zixuanli3566 <li3566@purdue.edu>
2021-08-20 00:10:54 -0400

    minor changes

--- testForPriority.py
+++ testForPriority.py
@@ -9,7 +9,7 @@ from mininet.examples.cluster import ( MininetCluster, SwitchBinPlacer,
 
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-from mininet.link import TCLink
+from mininet.link import TCLink, TCIntf
 
 import mininet.util
 
@@ -43,11 +43,11 @@ class MyTopo(Topo):
         # print(leftHost)
         # print(type(leftHost))
         # Add links
-        self.addLink( leftHost, leftSwitch,  )
-        self.addLink( rightSwitch, rightHost )
-        self.addLink( leftSwitch, ldownHost  )
+        self.addLink( leftHost, leftSwitch)
+        self.addLink( rightSwitch, rightHost)
+        self.addLink( leftSwitch, ldownHost )
         self.addLink( rightSwitch, rdownHost )
-        self.addLink( leftSwitch, rightSwitch, bw = 1)
+        self.addLink( leftSwitch, rightSwitch)
     
     def get_switches(self):
         return self.switches()
@@ -58,46 +58,47 @@ class RulesHandler(object):
         self.ofctl_rules = defaultdict(list)
         self.vsctl_rules = defaultdict(list)
         self.generate_rules(net)
+
     def rules_for_pqueue(self, switch_name, switch_intf, switch_port):
-        cmd = "set port"
-        parameters = "{} qos=@newqos -- \
-                    --id=@newqos create qos type=linux-htb \
-                        max-rate=1000000000 \
-                        queues:{}1=@hpriority \
-                        queues:{}2=@lpriority -- \
-                    --id=@hpriority create queue other-config:priority=10,max-rate=1000000000 -- \
-                    --id=@lpriority create queue other-config:priority=20,max-rate=1000000000".format(switch_intf, str(switch_port), str(switch_port))
+        cmd = "set"
+        parameters = " port {} qos=@newqos -- --id=@newqos create qos type=linux-htb max-rate=1000000000 queues:{}1=@hpriority queues:{}2=@lpriority -- --id=@hpriority create queue other-config:priority=10,max-rate=1000 -- --id=@lpriority create queue other-config:priority=20,max-rate=1000".format(switch_intf, str(switch_port), str(switch_port))
+        
         self.vsctl_rules[switch_name].append((cmd, parameters))
-        cmd = "add-flow"
-        parameters = "in_port=1,actions=set_queue:{}1".format(str(switch_port))
-        self.ofctl_rules[switch_name].append((cmd, parameters))
-        cmd = "add-flow"
-        parameters = "in_port=2,actions=set_queue:{}2".format(str(switch_port))
-        self.ofctl_rules[switch_name].append((cmd, parameters))
+        
+
     def rules_for_forward(self, switch_name, net):
-        cmd = "add-flow"
-        parameters = "in_port=1,actions=output:3"
+        cmd = "-O OpenFlow13 add-flow"
+        parameters = \
+                "\"table=0,in_port=1,actions=push_mpls:0x8847,set_field:100->mpls_label,set_queue:31,output:3\""
         self.ofctl_rules[switch_name].append((cmd, parameters))
-        cmd = "add-flow"
-        parameters = "in_port=2,actions=output:3"
+        cmd = "-O OpenFlow13 add-flow"
+        parameters = \
+                "\"table=0,in_port=2,actions=push_mpls:0x8847,set_field:100->mpls_label,set_queue:32,output:3\""
         self.ofctl_rules[switch_name].append((cmd, parameters))
+       
         if switch_name == 's1':
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=0,in_port=3,mpls,mpls_label=100,eth_type=0x8847,actions=pop_mpls:0x800,goto_table:1"
+            self.ofctl_rules[switch_name].append((cmd, parameters))
             h1 = net.getNodeByName('h1')
-            cmd = "add-flow"
-            parameters = "in_port=3,ip,ip_dst={},actions=output:1".format(h1.IP())
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=1,ip,ip_dst={},actions=output:1".format(h1.IP())
             self.ofctl_rules[switch_name].append((cmd, parameters))
             h3 = net.getNodeByName('h3')
-            cmd = "add-flow"
-            parameters = "in_port=3,ip,ip_dst={},actions=output:2".format(h3.IP())
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=1,ip,ip_dst={},actions=output:2".format(h3.IP())
             self.ofctl_rules[switch_name].append((cmd, parameters))
         elif switch_name == 's2':
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=0,in_port=3,mpls,mpls_label=100,eth_type=0x8847,actions=pop_mpls:0x800,goto_table:1"
+            self.ofctl_rules[switch_name].append((cmd, parameters))
             h2 = net.getNodeByName('h2')
-            cmd = "add-flow"
-            parameters = "in_port=3,ip,ip_dst={},actions=output:1".format(h2.IP())
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=1,in_port=3,ip,ip_dst={},actions=output:1".format(h2.IP())
             self.ofctl_rules[switch_name].append((cmd, parameters))
             h4 = net.getNodeByName('h4')
-            cmd = "add-flow"
-            parameters = "in_port=3,ip,ip_dst={},actions=output:2".format(h4.IP())
+            cmd = "-O OpenFlow13 add-flow"
+            parameters = "table=1,in_port=3,ip,ip_dst={},actions=output:2".format(h4.IP())
             self.ofctl_rules[switch_name].append((cmd, parameters))
     def generate_rules(self, net):
         # topo.links: [('s2', 'h2', {'node1': 's2', 'node2': 'h2', 'port2': 0, 'port1': 2}), ('s1', 's2', {'node1': 's1', 'node2': 's2', 'port2': 1, 'port1': 2}), ('h1', 's1', {'node1': 'h1', 'node2': 's1', 'port2': 20, 'port1': 10}), ('s2', 'h4', {'node1': 's2', 'node2': 'h4', 'port2': 0, 'port1': 3}), ('s1', 'h3', {'node1': 's1', 'node2': 'h3', 'port2': 0, 'port1': 3})]
@@ -126,13 +127,19 @@ class RulesHandler(object):
     
     def implement_rules(self, net):
         print("Implementing vsctl rules")
-        process_list = []
+        # process_list = []
+        # for switch in self.vsctl_rules:
+        #     p = Process(target=self._implement_switch_rules, args=(net.getNodeByName(switch), self.vsctl_rules[switch], 'vsctl'))
+        #     process_list.append(p)
+        #     p.start()
+        # for p in process_list:
+        #     p.join()
         for switch in self.vsctl_rules:
-            p = Process(target=self._implement_switch_rules, args=(net.getNodeByName(switch), self.vsctl_rules[switch], 'vsctl'))
-            process_list.append(p)
-            p.start()
-        for p in process_list:
-            p.join()
+            s = net.getNodeByName(switch)
+            cmd = "ovs-vsctl set port {}-eth3 qos=@newqos -- --id=@newqos create qos type=linux-htb other-config:max-rate=500000 queues:31=@hpriority queues:32=@lpriority -- --id=@hpriority create queue other-config:priority=10,max-rate=50000 -- --id=@lpriority create queue other-config:priority=20,max-rate=50000".format(switch)
+            print(cmd)
+            s.cmd(cmd)
+        
         print("Implementing ofctl rules")
         process_list = []
         for switch in self.ofctl_rules:
@@ -148,7 +155,8 @@ class TestBed(object):
         self.net = Mininet(
             topo = self.topo,
             switch=mininet.node.OVSSwitch,
-            link=TCLink,
+
+            # intf=TCIntf,
             controller=None,
             cleanup=True,
             autoSetMacs=True, 
@@ -174,7 +182,7 @@ class TestBed(object):
         h3 = self.net.getNodeByName('h3')
         h4 = self.net.getNodeByName('h4')
         self.iperfPair(client = h1, server = h2, bw = 2000, num_session = 1, port = 5000)
-        self.iperfPair(client = h3, server = h4, bw = 400, num_session = 1, port = 5001)
+        # self.iperfPair(client = h3, server = h4, bw = 2000, num_session = 1, port = 5001)
         
 
     def start(self):
@@ -189,8 +197,11 @@ class TestBed(object):
         # self.net.ping(hosts=[h1, h2])
         # self.net.ping(hosts=[h3, h4])
         # self.net.ping(hosts=[h1, h3])
-        # cli = CLI(self.net)
-        # cli.run()
+        # s1 = self.net.getNodeByName('s1')
+        # print(s1.intf())
+        # print(type(s1.intf('s1-eth3')))
+        cli = CLI(self.net)
+        cli.run()
         # self.testThrpt()
         print("Done.")
         
@@ -216,3 +227,31 @@ class TestBed(object):
 test_bed = TestBed()
 time.sleep(5)
 test_bed.start()
+
+"""
+"ovs-vsctl -O OpenFlow13 set port s1-eth3 qos=@newqos -- \
+                    --id=@newqos create qos type=linux-htb \
+                        queues:31=@hpriority \
+                        queues:32=@lpriority -- \
+                    --id=@hpriority create queue other-config:priority=10,max-rate=1000 -- \
+                    --id=@lpriority create queue other-config:priority=20,max-rate=1000"
+
+$ ovs-vsctl -- \
+  set port s1-eth3 qos=@newqos -- \
+  --id=@newqos create qos type=linux-htb \
+      other-config:max-rate=1000000000 \
+      queues:123=@vif10queue \
+      queues:234=@vif20queue -- \
+  --id=@vif10queue create queue other-config:max-rate=10000000 -- \
+  --id=@vif20queue create queue other-config:max-rate=20000000
+
+ovs-ofctl queue-stats
+ovs-ofctl dump-flows
+
+
+ovs-ofctl -O OpenFlow13 add-flow s1 in_port=1,actions=set_queue:31
+
+ovs-ofctl -O OpenFlow13 add-flow s1 table=1,in_port=3,ip,ip_dst=10.0.0.4,actions=output:2
+"""
+
+
